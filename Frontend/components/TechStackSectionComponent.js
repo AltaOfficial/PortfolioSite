b03478a1f5e5272fd22fd5b0ae44e2fe461278f9
jaedon farr <jaedonfarr@gmail.com>
2024-11-14 16:55:06 -0500

    Update TechStackSectionComponent.js
    added responsiveness to tech stack section

--- Frontend/components/TechStackSectionComponent.js
+++ Frontend/components/TechStackSectionComponent.js
@@ -21,15 +21,20 @@ export default function TechStackSectionComponent({ techStacksRef }) {
 
   return (
     <div className="mt-32 ml-5" id="techStacks" ref={techStacksRef}>
-      <p className="text-6xl font-semibold mb-10">
+      <p className="lg:text-6xl text-4xl font-semibold mb-10">
         My Tech Stack and Experience
       </p>
       <div>
-        <div className="h-96 grid grid-cols-4">
+        <div className="h-96 grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 md:gap-2 sm:place-items-center xl:place-items-start lg:place-items-center place-items-center">
           {techCategories &&
             techCategories.map((techCategory, index) => (
-              <div key={index} className="flex flex-col gap-4">
-                <p className="text-3xl mb-2 font-semibold">{techCategory} -</p>
+              <div
+                key={index}
+                className="flex flex-col gap-4 place-items-center"
+              >
+                <p className="lg:text-3xl text-xl mb-2 font-semibold">
+                  {techCategory}
+                </p>
                 {techStacks &&
                   techStacks
                     .filter(
